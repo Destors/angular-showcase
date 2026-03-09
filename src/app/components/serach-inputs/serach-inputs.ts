17b30fe87c396fdf7b6e27a8b5e79e49621a484b
Destors <oleksandrprosolovych@gmail.com>
2026-03-09 18:09:39 +0200

    rxjs searhc

--- src/app/components/serach-inputs/serach-inputs.ts
+++ src/app/components/serach-inputs/serach-inputs.ts
@@ -1,3 +1,4 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import {
   debounceTime,
@@ -7,25 +8,25 @@ import {
   map,
   Observable,
   of,
+  Subject,
   switchMap,
 } from 'rxjs';
 
 @Component({
   selector: 'app-serach-inputs',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './serach-inputs.html',
   styleUrl: './serach-inputs.css',
 })
 export class SerachInputs implements AfterViewInit {
   @ViewChild('searchInput') inputElement!: ElementRef<HTMLInputElement>;
-  apiCallResult: string = '';
-
+  apiCallResult$: Subject<string> = new Subject<string>();
   searchText$!: Observable<string>;
 
   ngAfterViewInit() {
     this.initSearch();
     this.searchText$.subscribe((result) => {
-      this.apiCallResult = result;
+      this.apiCallResult$.next(result);
       console.log('API Call Result:', result);
     });
   }
@@ -42,10 +43,5 @@ export class SerachInputs implements AfterViewInit {
   mockApiCall(searchText: string): Observable<string> {
     // Simulate an API call with a delay
     return of(`Results for "${searchText}"`).pipe(delay(500));
-  }
-
-  onSearch(value: string) {
-    console.log('Search value:', value);
-    this.apiCallResult = `Results for "${value}"`;
   }
 }
